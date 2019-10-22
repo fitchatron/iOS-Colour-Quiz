43daab55f879d79466235fa49c37c361ba57a2c1
James Fitch <fitchy@me.com>
2019-10-22 22:37:02 +1100

    Initial Commit

---
+++ ColourQuiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColourQuiz
+//
+//  Created by James Fitch on 22/10/19.
+//  Copyright © 2019 Fitchatron. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
