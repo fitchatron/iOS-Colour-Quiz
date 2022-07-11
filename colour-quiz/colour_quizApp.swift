e5c5264aef6fdcf155b912746781527d6a98cbbf
fitchatron <53107122+fitchatron@users.noreply.github.com>
2022-07-11 10:40:10 +1000

    initial commit v2

---
+++ colour-quiz/colour_quizApp.swift
@@ -0,0 +1,17 @@
+//
+//  colour_quizApp.swift
+//  colour-quiz
+//
+//  Created by James Fitch on 11/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct colour_quizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
